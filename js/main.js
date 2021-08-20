2686374b865ca46de9e4ea2e165f559bd6ab0da8
jcvillanq <55392743+jcvillanq@users.noreply.github.com>
2021-08-20 20:35:06 +0200

    20/08/2021

--- js/main.js
+++ js/main.js
@@ -4,7 +4,3 @@ M.AutoInit();
     
 });
 
-
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
